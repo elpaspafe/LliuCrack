53a434e481290b4e447b8f62af803e4e6f945068
Jose <elpaspafe@gmail.com>
2023-02-21 15:27:58 +0100

    Python file

---
+++ 4passCracker.py
@@ -0,0 +1,17 @@
+import itertools
+
+# Definimos los elementos a combinar
+elements = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+# Definimos el tamaño máximo de cada combinación
+combination_length = 4
+
+# Abrimos el archivo en modo de escritura
+with open('RockLliu.txt', 'w') as file:
+
+    # Generamos todas las posibles combinaciones
+    combinations = itertools.product(elements, repeat=combination_length)
+
+    # Escribimos las combinaciones en el archivo
+    for combination in combinations:
+        file.write(''.join(combination) + '\n')
